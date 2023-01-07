fb1a9b54f29270ebbca3f942047d5bdfb6855276
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-01-07 17:37:32 +0000

    feat: add initial files following npm create vite@latest . -- --template react-ts then https://tailwindcss.com/docs/guides/vite

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## How this repo was started
+- Create React, Typescript, Vite starter files: `npm create vite@latest . -- --template react-ts`, following https://vitejs.dev/guide/
+- Install tailwind, following: https://tailwindcss.com/docs/guides/vite
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold">
+      React + Typescript + Tailwind + Vite + pnpm
+    </h1>
+    </div>
+  )
+}
+
+export default App
